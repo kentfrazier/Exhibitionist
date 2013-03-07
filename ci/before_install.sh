5a7ba2b3a8777c50e5cdd9543bc56e3203fdb164
y-p <yoval@gmx.com>
2013-03-07 23:14:56 +0200

    TRAVIS: install grequests only on python2

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -2,10 +2,16 @@
 
 echo "inside $0"
 
+sudo apt-get install libevent-1.4-2 libevent-dev
+
 # these are requirements for TESTING only
 pip install -M nose
+if [ ${TRAVIS_PYTHON_VERSION:0:1} == "2" ] ; then
+    pip install -M gevent
+    pip install -M grequests
+fi
+
 pip install -M requests
-pip install -M grequests
 pip install -M coverage
 
 # waiting on  https://github.com/Lawouach/WebSocket-for-Python/pull/81
